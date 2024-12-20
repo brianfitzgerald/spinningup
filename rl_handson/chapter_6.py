f71fca904d74704e835ab1b4beb2f02b670ef32d
Brian Fitzgerald <brianfitzgerald242@gmail.com>
2024-12-20 14:07:01 -0600

    chapter 6 impl

--- rl_handson/chapter_6.py
+++ rl_handson/chapter_6.py
@@ -38,16 +38,29 @@ Algorithm is:
 
 """
 
+import time
+from collections import deque
+from dataclasses import dataclass
+from typing import List, Optional, Tuple
+
 import fire
 import gymnasium
-from tensorboardX import SummaryWriter
+import numpy as np
+import torch
+import torch.nn as nn
 from gymnasium import Env
 from gymnasium.wrappers import RecordVideo
-from collections import defaultdict, Counter
-import torch.nn as nn
-import torch
+from tensorboardX import SummaryWriter
+from torch.optim import Adam
 
-from stable_baselines3.common.atari_wrappers import MaxAndSkipEnv
+BatchTensors = Tuple[
+    torch.ByteTensor,  # current state
+    torch.LongTensor,  # actions
+    torch.Tensor,  # rewards
+    torch.BoolTensor,  # done || trunc
+    torch.ByteTensor,  # next state
+]
+
 
 class DQN(nn.Module):
 
@@ -61,22 +74,232 @@ class DQN(nn.Module):
             nn.Conv2d(32, 64, kernel_size=4, stride=2),
             nn.ReLU(),
             nn.Conv2d(64, 64, kernel_size=3, stride=1),
-            nn.ReLU()
+            nn.ReLU(),
         )
         # get the size of the output of the conv layer
-        size = self.conv(torch.zeros(1, *input_shape)).view(1, -1).size(1)
+        size = self.conv(torch.zeros(1, *input_shape)).size()[-1]
         # output q values for each action
         self.fc = nn.Sequential(
-            nn.Linear(size, 512),
-            nn.ReLU(),
-            nn.Linear(512, n_actions)
+            nn.Linear(size, 512), nn.ReLU(), nn.Linear(512, n_actions)
         )
 
-def main(q_iteration=True):
+    def forward(self, x: torch.ByteTensor):
+        # scale on GPU
+        xx = x / 255.0
+        return self.fc(self.conv(xx))
 
-    env = gymnasium.make(ENV_NAME, render_mode="rgb_array")
+
+class ExperienceBuffer:
+    def __init__(self, capacity):
+        self.buffer = deque(maxlen=capacity)
+
+    def __len__(self):
+        return len(self.buffer)
+
+    def append(self, experience):
+        self.buffer.append(experience)
+
+    def sample(self, batch_size):
+        # Sample a random batch of experiences, and return as a tuple of arrays
+        indices = np.random.choice(len(self.buffer), batch_size, replace=False)
+        states, actions, rewards, dones, next_states = zip(
+            *[self.buffer[idx] for idx in indices]
+        )
+        return (
+            np.array(states),
+            np.array(actions),
+            np.array(rewards, dtype=np.float32),
+            np.array(dones, dtype=np.uint8),
+            np.array(next_states),
+        )
+
+
+State = np.ndarray
+Action = int
+
+
+@dataclass
+class Experience:
+    state: State
+    action: Action
+    reward: float
+    done_trunc: bool
+    new_state: State
+
+
+class Agent:
+    def __init__(self, env: Env, exp_buffer: ExperienceBuffer):
+        self.env = env
+        self.exp_buffer = exp_buffer
+        self.state: Optional[np.ndarray] = None
+        self._reset()
+
+    def _reset(self):
+        self.state, _ = self.env.reset()
+        self.total_reward = 0.0
+
+    @torch.no_grad()
+    def play_step(
+        self, net: DQN, device: torch.device, epsilon: float = 0.0
+    ) -> Optional[float]:
+        done_reward = None
+
+        if np.random.random() < epsilon:
+            action = self.env.action_space.sample()
+        else:
+            # Get the max Q-value and perform that action
+            state_v = torch.as_tensor(self.state).to(device)
+            state_v.unsqueeze_(0)
+            q_vals_v = net(state_v)
+            _, act_v = torch.max(q_vals_v, dim=1)
+            action = int(act_v.item())
+
+        # do step in the environment
+        new_state, reward, is_done, is_tr, _ = self.env.step(action)
+        self.total_reward += reward
+
+        exp = Experience(
+            state=self.state,
+            action=action,
+            reward=float(reward),
+            done_trunc=is_done or is_tr,
+            new_state=new_state,
+        )
+        self.exp_buffer.append(exp)
+        self.state = new_state
+        if is_done or is_tr:
+            done_reward = self.total_reward
+            self._reset()
+        return done_reward
+
+
+def batch_to_tensors(batch: List[Experience], device: torch.device) -> BatchTensors:
+    """
+    Convert a batch of experiences to tensors
+    """
+    states, actions, rewards, dones, new_state = [], [], [], [], []
+    for e in batch:
+        states.append(e.state)
+        actions.append(e.action)
+        rewards.append(e.reward)
+        dones.append(e.done_trunc)
+        new_state.append(e.new_state)
+    states_t = torch.as_tensor(np.asarray(states))
+    actions_t = torch.LongTensor(actions)
+    rewards_t = torch.FloatTensor(rewards)
+    dones_t = torch.BoolTensor(dones)
+    new_states_t = torch.as_tensor(np.asarray(new_state))
+    return (
+        states_t.to(device),
+        actions_t.to(device),
+        rewards_t.to(device),
+        dones_t.to(device),
+        new_states_t.to(device),
+    )
+
+
+DEFAULT_ENV_NAME = "ALE/Pong-v5"
+MEAN_REWARD_BOUND = 19.5
+
+GAMMA = 0.99
+BATCH_SIZE = 32
+REPLAY_SIZE = 10000
+LEARNING_RATE = 1e-4
+SYNC_TARGET_FRAMES = 1000
+REPLAY_START_SIZE = 10000
+
+# Decay epsilon from start to final in the first 10^5 frames
+# This means we start with a high exploration rate, and then decrease it
+EPSILON_DECAY_LAST_FRAME = 10**5
+EPSILON_START = 1.0
+EPSILON_FINAL = 0.02
+
+
+def calc_loss(
+    batch: List[Experience], net: DQN, tgt_net: DQN, device: torch.device
+) -> torch.Tensor:
+
+    # Get experiences as batch of tensors
+    states_t, actions_t, rewards_t, dones_t, new_states_t = batch_to_tensors(
+        batch, device
+    )
+
+    # get the action value pairs for the current state
+    state_action_values = net(states_t).gather(1, actions_t.unsqueeze(-1)).squeeze(-1)
+    with torch.no_grad():
+        next_state_values = tgt_net(new_states_t).max(1)[0]
+        # set the next state values to 0 if the episode is done
+        next_state_values[dones_t] = 0.0
+        next_state_values = next_state_values.detach()
+
+    # scale the expected rewards by gamma and add to the next state values
+    expected_state_action_values = next_state_values * GAMMA + rewards_t
+    # calculate the loss between the predicted and expected state action values
+    return nn.MSELoss()(state_action_values, expected_state_action_values)
+
+
+def main(device_str: str = "mps", env: str = DEFAULT_ENV_NAME):
+
+    device = torch.device(device_str)
+
+    env = gymnasium.make(DEFAULT_ENV_NAME, render_mode="rgb_array")
     env = RecordVideo(env, video_folder=f"videos/chapter_5")
+
+    net = DQN(env.observation_space.shape, env.action_space.n).to(device)
+    tgt_net = DQN(env.observation_space.shape, env.action_space.n).to(device)
+
+    buffer = ExperienceBuffer(REPLAY_SIZE)
+    agent = Agent(env, buffer)
+    epsilon = EPSILON_START
+
+    optimizer = Adam(net.parameters(), lr=LEARNING_RATE)
+    total_rewards = []
+    frame_idx = 0
+    ts_frame = 0
+    ts = time.time()
+    best_m_reward = None
+
     writer = SummaryWriter(comment="-v-iteration")
+
+    while True:
+        frame_idx += 1
+        epsilon = max(
+            EPSILON_FINAL, EPSILON_START - frame_idx / EPSILON_DECAY_LAST_FRAME
+        )
+
+        reward = agent.play_step(net, device, epsilon)
+        if reward is not None:
+            total_rewards.append(reward)
+            speed = (frame_idx - ts_frame) / (time.time() - ts)
+            ts_frame = frame_idx
+            ts = time.time()
+            m_reward = np.mean(total_rewards[-100:])
+            print(
+                f"{frame_idx}: done {len(total_rewards)} games, reward {m_reward:.3f}, "
+                f"eps {epsilon:.2f}, speed {speed:.2f} f/s"
+            )
+            writer.add_scalar("epsilon", epsilon, frame_idx)
+            writer.add_scalar("speed", speed, frame_idx)
+            writer.add_scalar("reward_100", m_reward, frame_idx)
+            writer.add_scalar("reward", reward, frame_idx)
+            if best_m_reward is None or best_m_reward < m_reward:
+                torch.save(net.state_dict(), env + "-best_%.0f.dat" % m_reward)
+                if best_m_reward is not None:
+                    print(f"Best reward updated {best_m_reward:.3f} -> {m_reward:.3f}")
+                best_m_reward = m_reward
+            if m_reward > MEAN_REWARD_BOUND:
+                print("Solved in %d frames!" % frame_idx)
+                break
+        if len(buffer) < REPLAY_START_SIZE:
+            continue
+        if frame_idx % SYNC_TARGET_FRAMES == 0:
+            tgt_net.load_state_dict(net.state_dict())
+
+        optimizer.zero_grad()
+        batch = buffer.sample(BATCH_SIZE)
+        loss_t = calc_loss(batch, net, tgt_net, device)
+        loss_t.backward()
+        optimizer.step()
 
     writer.close()
 
