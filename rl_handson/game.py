161fe65f632c65e0cc64b9c93ee07dd6219ced7b
Brian Fitzgerald <brianfitzgerald242@gmail.com>
2025-01-04 16:26:23 -0600

    add game and mcts

---
+++ rl_handson/game.py
@@ -0,0 +1,145 @@
+class ConnectFour:
+    def __init__(self, rows=6, cols=7, bits_in_len=3, count_to_win=4):
+        self.rows = rows
+        self.cols = cols
+        self.bits_in_len = bits_in_len
+        self.count_to_win = count_to_win
+        self.player_black = 1
+        self.player_white = 0
+        self.initial_state = self.encode_lists([[]] * self.cols)
+
+    def bits_to_int(self, bits):
+        res = 0
+        for b in bits:
+            res *= 2
+            res += b
+        return res
+
+    def int_to_bits(self, num, bits):
+        res = []
+        for _ in range(bits):
+            res.append(num % 2)
+            num //= 2
+        return res[::-1]
+
+    def encode_lists(self, field_lists):
+        """
+        Encode lists representation into the binary numbers
+        :param field_lists: list of self.cols lists with 0s and 1s
+        :return: integer number with encoded game state
+        """
+        assert isinstance(field_lists, list)
+        assert len(field_lists) == self.cols
+
+        bits = []
+        len_bits = []
+        for col in field_lists:
+            bits.extend(col)
+            free_len = self.rows - len(col)
+            bits.extend([0] * free_len)
+            len_bits.extend(self.int_to_bits(free_len, bits=self.bits_in_len))
+        bits.extend(len_bits)
+        return self.bits_to_int(bits)
+
+    def decode_binary(self, state_int):
+        """
+        Decode binary representation into the list view
+        :param state_int: integer representing the field
+        :return: list of self.cols lists
+        """
+        assert isinstance(state_int, int)
+        bits = self.int_to_bits(state_int, bits=self.cols * self.rows + self.cols * self.bits_in_len)
+        res = []
+        len_bits = bits[self.cols * self.rows:]
+        for col in range(self.cols):
+            vals = bits[col * self.rows:(col + 1) * self.rows]
+            lens = self.bits_to_int(len_bits[col * self.bits_in_len:(col + 1) * self.bits_in_len])
+            if lens > 0:
+                vals = vals[:-lens]
+            res.append(vals)
+        return res
+
+    def possible_moves(self, state_int):
+        """
+        This function could be calculated directly from bits, but I'm too lazy
+        :param state_int: field representation
+        :return: the list of columns which we can make a move
+        """
+        assert isinstance(state_int, int)
+        field = self.decode_binary(state_int)
+        return [idx for idx, col in enumerate(field) if len(col) < self.rows]
+
+    def _check_won(self, field, col, delta_row):
+        """
+        Check for horizontal/diagonal win condition for the last player moved in the column
+        :param field: list of lists
+        :param col: column index
+        :param delta_row: if 0, checks for horizontal won, 1 for rising diagonal, -1 for falling
+        :return: True if won, False if not
+        """
+        player = field[col][-1]
+        coord = len(field[col]) - 1
+        total = 1
+        # negative dir
+        cur_coord = coord - delta_row
+        for c in range(col - 1, -1, -1):
+            if len(field[c]) <= cur_coord or cur_coord < 0 or cur_coord >= self.rows:
+                break
+            if field[c][cur_coord] != player:
+                break
+            total += 1
+            if total == self.count_to_win:
+                return True
+            cur_coord -= delta_row
+        # positive dir
+        cur_coord = coord + delta_row
+        for c in range(col + 1, self.cols):
+            if len(field[c]) <= cur_coord or cur_coord < 0 or cur_coord >= self.rows:
+                break
+            if field[c][cur_coord] != player:
+                break
+            total += 1
+            if total == self.count_to_win:
+                return True
+            cur_coord += delta_row
+        return False
+
+    def move(self, state_int, col, player):
+        """
+        Perform move into given column. Assume the move could be performed, otherwise, assertion will be raised
+        :param state_int: current state
+        :param col: column to make a move
+        :param player: player index (player_white or player_black)
+        :return: tuple of (state_new, won). Value won is bool, True if this move lead
+        to victory or False otherwise (but it could be a draw)
+        """
+        assert isinstance(state_int, int)
+        assert isinstance(col, int)
+        assert 0 <= col < self.cols
+        assert player == self.player_black or player == self.player_white
+        field = self.decode_binary(state_int)
+        assert len(field[col]) < self.rows
+        field[col].append(player)
+        # check for victory: the simplest vertical case
+        suff = field[col][-self.count_to_win:]
+        won = suff == [player] * self.count_to_win
+        if not won:
+            won = (self._check_won(field, col, 0) or 
+                  self._check_won(field, col, 1) or 
+                  self._check_won(field, col, -1))
+        state_new = self.encode_lists(field)
+        return state_new, won
+
+    def render(self, state_int):
+        state_list = self.decode_binary(state_int)
+        data = [[' '] * self.cols for _ in range(self.rows)]
+        for col_idx, col in enumerate(state_list):
+            for rev_row_idx, cell in enumerate(col):
+                row_idx = self.rows - rev_row_idx - 1
+                data[row_idx][col_idx] = str(cell)
+        return [''.join(row) for row in data]
+
+    def update_counts(self, counts_dict, key, counts):
+        v = counts_dict.get(key, (0, 0, 0))
+        res = (v[0] + counts[0], v[1] + counts[1], v[2] + counts[2])
+        counts_dict[key] = res
---
+++ rl_handson/muzero.py
@@ -0,0 +1,130 @@
+import torch
+import torch.nn as nn
+
+import numpy as np
+from typing import Tuple, Optional
+
+class Net(nn.Module):
+    def __init__(self, input_shape, actions_n, num_filters):
+        super(Net, self).__init__()
+
+        conv_blocks = [
+            # Input conv
+            nn.Conv2d(input_shape[0], num_filters, kernel_size=3, padding=1),
+            nn.BatchNorm2d(num_filters),
+            nn.LeakyReLU(),
+        ]
+
+        # Add 5 residual conv blocks
+        for _ in range(5):
+            conv_blocks.extend(
+                [
+                    nn.Conv2d(num_filters, num_filters, kernel_size=3, padding=1),
+                    nn.BatchNorm2d(num_filters),
+                    nn.LeakyReLU(),
+                ]
+            )
+
+        self.conv_layers = nn.Sequential(*conv_blocks)
+
+        body_shape = (num_filters,) + input_shape[1:]
+
+        # value head
+        self.conv_val = nn.Sequential(
+            nn.Conv2d(num_filters, 1, kernel_size=1),
+            nn.BatchNorm2d(1),
+            nn.LeakyReLU(),
+            nn.Flatten(),
+        )
+        size = self.conv_val(torch.zeros(1, *body_shape)).size()[-1]
+        self.value = nn.Sequential(
+            nn.Linear(size, 20), nn.LeakyReLU(), nn.Linear(20, 1), nn.Tanh()
+        )
+
+        # policy head
+        self.conv_policy = nn.Sequential(
+            nn.Conv2d(num_filters, 2, kernel_size=1),
+            nn.BatchNorm2d(2),
+            nn.LeakyReLU(),
+            nn.Flatten(),
+        )
+        size: int = self.conv_policy(torch.zeros(1, *body_shape)).size()[-1]
+        self.policy = nn.Sequential(nn.Linear(size, actions_n))
+
+    def forward(self, x):
+        # Apply conv layers with residual connections
+        v = x
+        for i in range(
+            0, len(self.conv_layers), 3
+        ):  # Step by 3 since each block has 3 layers
+            if i == 0:
+                v = self.conv_layers[i : i + 3](v)  # First block (input conv)
+            else:
+                v = v + self.conv_layers[i : i + 3](v)  # Residual blocks
+
+        val = self.conv_val(v)
+        pol = self.conv_policy(v)
+        return pol, val
+
+
+def play_game(mcts_stores: Optional[Union[mcts.MCTS, List[mcts.MCTS]]],
+                replay_buffer: Optional[collections.deque], net1: Net, net2: Net,
+                steps_before_tau_0: int, mcts_searches: int, mcts_batch_size: int,
+                net1_plays_first: Optional[bool] = None,
+                device: torch.device = torch.device("cpu")):
+    """
+    Play one single game, memorizing transitions into the replay buffer
+    :param mcts_stores: could be None or single MCTS or two MCTSes for individual net
+    :param replay_buffer: queue with (state, probs, values), if None, nothing is stored
+    :param net1: player1
+    :param net2: player2
+    :return: value for the game in respect to player1 (+1 if p1 won, -1 if lost, 0 if draw)
+    """
+    if mcts_stores is None:
+        mcts_stores = [mcts.MCTS(), mcts.MCTS()]
+    elif isinstance(mcts_stores, mcts.MCTS):
+        mcts_stores = [mcts_stores, mcts_stores]
+
+    state = game.INITIAL_STATE
+    nets = [net1, net2]
+    if net1_plays_first is None:
+        cur_player = np.random.choice(2)
+    else:
+        cur_player = 0 if net1_plays_first else 1
+    step = 0
+    tau = 1 if steps_before_tau_0 > 0 else 0
+    game_history = []
+
+    result = None
+    net1_result = None
+
+    while result is None:
+        mcts_stores[cur_player].search_batch(
+            mcts_searches, mcts_batch_size, state,
+            cur_player, nets[cur_player], device=device)
+        probs, _ = mcts_stores[cur_player].get_policy_value(state, tau=tau)
+        game_history.append((state, cur_player, probs))
+        action = np.random.choice(game.GAME_COLS, p=probs)
+        if action not in game.possible_moves(state):
+            print("Impossible action selected")
+        state, won = game.move(state, action, cur_player)
+        if won:
+            result = 1
+            net1_result = 1 if cur_player == 0 else -1
+            break
+        cur_player = 1-cur_player
+        # check the draw case
+        if len(game.possible_moves(state)) == 0:
+            result = 0
+            net1_result = 0
+            break
+        step += 1
+        if step >= steps_before_tau_0:
+            tau = 0
+
+    if replay_buffer is not None:
+        for state, cur_player, probs in reversed(game_history):
+            replay_buffer.append((state, cur_player, probs, result))
+            result = -result
+
+    return net1_result, step
