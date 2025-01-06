093127e3ae7c539f293a17f9b9e29642a047bbf4
Brian Fitzgerald <brianfitzgerald242@gmail.com>
2025-01-05 18:24:02 -0600

    wip - training loop

--- rl_handson/chapter_20_muzero.py
+++ rl_handson/chapter_20_muzero.py
@@ -19,25 +19,21 @@ Tree uses upper confidence bounds instead of the value of the state
 """
 
 import os
-import random
 from time import time
 from collections import deque
+from itertools import chain
 
-import fire
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from game import ConnectFour
 from lib import ensure_directory, get_device
-from loguru import logger
-from mcts import MCTS, state_lists_to_batch
-from rl_handson.alphazero import Net, play_game
-from ptan import (
-    TargetNet,
-    TBMeanTracker,
-)
+from mcts import MCTS
+from rl_handson.alphazero import play_game
 from torch.utils.tensorboard.writer import SummaryWriter
+
+from muzero import sample_batch, play_game, MuZeroModels, MuZeroParams
 
 
 # count of recent episodes in the replay buffer
@@ -82,81 +78,98 @@ def evaluate(
 
 
 def main(name: str = "mcts"):
-    device = get_device()
+    device = torch.device(get_device())
     saves_path = os.path.join("saves", name)
     ensure_directory(saves_path)
     writer = SummaryWriter(comment="-" + name)
 
     game = ConnectFour()
-    # format is (state, cur_player, probs, result)
+    hidden_size = 64
+
+    net = MuZeroModels(game.obs_shape, game.cols, hidden_size)
+    net.to(device)
+    best_net = MuZeroModels(game.obs_shape, game.cols, hidden_size)
+    best_net.to(device)
+    max_moves = game.rows * game.cols >> 2 + 1
+    params = MuZeroParams(game.cols, max_moves, dev=device)
+
+    optimizer = optim.SGD(
+        chain(
+            net.repr.parameters(),
+            net.pred.parameters(),
+            net.dynamics.parameters(),
+        ),
+        lr=LEARNING_RATE,
+        momentum=0.9,
+    )
+
     replay_buffer = deque(maxlen=REPLAY_BUFFER)
+    temperature = TEMP_START
     step_idx = 0
     best_idx = 0
 
-    with TBMeanTracker(writer, batch_size=10) as tb_tracker:
-        while True:
-            step_idx += 1
+    while True:
+        step_idx += 1
+        ts = time()
+        score, episode = play_game(best_net, best_net, params, temperature=temperature)
+        print(f"{step_idx}: result {score}, steps={len(episode)}")
+        replay_buffer.append(episode)
+        writer.add_scalar("time_play", time() - ts, step_idx)
+        writer.add_scalar("steps", len(episode), step_idx)
+
+        # training
+        ts = time()
+        for _ in range(TRAIN_ROUNDS):
+            states_t, actions, policy_tgt, rewards_tgt, values_tgt = sample_batch(
+                replay_buffer, BATCH_SIZE, params
+            )
+
+            optimizer.zero_grad()
+            h_t = net.repr(states_t)
+            loss_p_full_t = None
+            loss_v_full_t = None
+            loss_r_full_t = None
+            for step in range(params.unroll_steps):
+                policy_t, values_t = net.pred(h_t)
+                loss_p_t = F.cross_entropy(policy_t, policy_tgt[step])
+                loss_v_t = F.mse_loss(values_t, values_tgt[step])
+                # dynamic step
+                rewards_t, h_t = net.dynamics(h_t, actions[step])
+                loss_r_t = F.mse_loss(rewards_t, rewards_tgt[step])
+                if step == 0:
+                    loss_p_full_t = loss_p_t
+                    loss_v_full_t = loss_v_t
+                    loss_r_full_t = loss_r_t
+                else:
+                    loss_p_full_t += loss_p_t * 0.5
+                    loss_v_full_t += loss_v_t * 0.5
+                    loss_r_full_t += loss_r_t * 0.5
+            assert loss_p_full_t is not None
+            loss_full_t = loss_v_full_t + loss_p_full_t + loss_r_full_t
+            loss_full_t.backward()
+            optimizer.step()
+
+            writer.add_scalar("loss_p", loss_p_full_t.item(), step_idx)  # type: ignore
+            writer.add_scalar("loss_v", loss_v_full_t.item(), step_idx)  # type: ignore
+            writer.add_scalar("loss_r", loss_r_full_t.item(), step_idx)  # type: ignore
+
+        writer.add_scalar("time_train", time() - ts, step_idx)
+        writer.add_scalar("temp", temperature, step_idx)
+        temperature = max(0.0, temperature - TEMP_STEP)
+
+        # evaluate net
+        if step_idx % EVALUATE_EVERY_STEP == 0:
             ts = time()
-            score, episode = play_game(best_net, best_net, params, temperature=temperature)
-            print(f"{step_idx}: result {score}, steps={len(episode)}")
-            replay_buffer.append(episode)
-            writer.add_scalar("time_play", time() - ts, step_idx)
-            writer.add_scalar("steps", len(episode), step_idx)
-
-            # training
-            ts = time()
-            for _ in range(TRAIN_ROUNDS):
-                states_t, actions, policy_tgt, rewards_tgt, values_tgt = \
-                    sample_batch(replay_buffer, BATCH_SIZE, params)
-
-                optimizer.zero_grad()
-                h_t = net.repr(states_t)
-                loss_p_full_t = None
-                loss_v_full_t = None
-                loss_r_full_t = None
-                for step in range(params.unroll_steps):
-                    policy_t, values_t = net.pred(h_t)
-                    loss_p_t = F.cross_entropy(policy_t, policy_tgt[step])
-                    loss_v_t = F.mse_loss(values_t, values_tgt[step])
-                    # dynamic step
-                    rewards_t, h_t = net.dynamics(h_t, actions[step])
-                    loss_r_t = F.mse_loss(rewards_t, rewards_tgt[step])
-                    if step == 0:
-                        loss_p_full_t = loss_p_t
-                        loss_v_full_t = loss_v_t
-                        loss_r_full_t = loss_r_t
-                    else:
-                        loss_p_full_t += loss_p_t * 0.5
-                        loss_v_full_t += loss_v_t * 0.5
-                        loss_r_full_t += loss_r_t * 0.5
-                loss_full_t = loss_v_full_t + loss_p_full_t + loss_r_full_t
-                loss_full_t.backward()
-                optimizer.step()
-
-                writer.add_scalar("loss_p", loss_p_full_t.item(), step_idx)
-                writer.add_scalar("loss_v", loss_v_full_t.item(), step_idx)
-                writer.add_scalar("loss_r", loss_r_full_t.item(), step_idx)
-
-
-            writer.add_scalar("time_train", time() - ts, step_idx)
-            writer.add_scalar("temp", temperature, step_idx)
-            temperature = max(0.0, temperature - TEMP_STEP)
-
-            # evaluate net
-            if step_idx % EVALUATE_EVERY_STEP == 0:
-                ts = time()
-                win_ratio, avg_steps = evaluate(net, best_net, params)
-                print("Net evaluated, win ratio = %.2f" % win_ratio)
-                writer.add_scalar("eval_win_ratio", win_ratio, step_idx)
-                writer.add_scalar("eval_steps", avg_steps, step_idx)
-                if win_ratio > BEST_NET_WIN_RATIO:
-                    print("Net is better than cur best, sync")
-                    best_net.sync(net)
-                    best_idx += 1
-                    file_name = os.path.join(saves_path, "best_%03d_%05d.dat" % (best_idx, step_idx))
-                    torch.save(best_net.get_state_dict(), file_name)
-                writer.add_scalar("time_eval", time() - ts, step_idx)
-
-
-if __name__ == "__main__":
-    fire.Fire(main)
+            win_ratio, avg_steps = evaluate(net, best_net, params)
+            print("Net evaluated, win ratio = %.2f" % win_ratio)
+            writer.add_scalar("eval_win_ratio", win_ratio, step_idx)
+            writer.add_scalar("eval_steps", avg_steps, step_idx)
+            if win_ratio > BEST_NET_WIN_RATIO:
+                print("Net is better than cur best, sync")
+                best_net.sync(net)
+                best_idx += 1
+                file_name = os.path.join(
+                    saves_path, "best_%03d_%05d.dat" % (best_idx, step_idx)
+                )
+                torch.save(best_net.get_state_dict(), file_name)
+            writer.add_scalar("time_eval", time() - ts, step_idx)
